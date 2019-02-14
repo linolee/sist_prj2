cca11ccbd39516f246ecc64dd95b4397c64ec943
eunyoung00 <beebee6656@gmail.com>
2019-02-14 10:21:19 +0900

    ㅜㅜㅜ

--- kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/controller/eunyoung.java
+++ kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/controller/eunyoung.java
@@ -3,7 +3,7 @@ package kr.co.sist.pcbang.manager.controller;
 public class eunyoung {
 
 	public static void main(String[] args) {
-		System.out.println("아니 안됀다ㅠㅠㅠ");
+		System.out.println("아니 안됀다ㅠㅠㅠnnnnn");
 	}
 
 }
