1527e285f66301ad826d964f3307aaa64b4de777
eunyoung00 <beebee6656@gmail.com>
2019-02-14 10:18:29 +0900

    쨔란

--- kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/controller/eunyoung.java
+++ kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/controller/eunyoung.java
@@ -3,8 +3,7 @@ package kr.co.sist.pcbang.manager.controller;
 public class eunyoung {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("µÈ´Ù~~");
 	}
 
 }
