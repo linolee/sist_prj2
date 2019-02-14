deac8a240fc42e0a3f995146197a32ba9d282617
Kim-JungWoon <45754698+Kim-JungWoon@users.noreply.github.com>
2019-02-14 10:20:32 +0900

    헬헿ㄹ헤헤헤헿헤헤헿헬헬헬

---
+++ kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/run/gotothehell.java
@@ -0,0 +1,10 @@
+package kr.co.sist.pcbang.manager.run;
+
+public class gotothehell {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ว๏วมนฬ");
+	}
+
+}
