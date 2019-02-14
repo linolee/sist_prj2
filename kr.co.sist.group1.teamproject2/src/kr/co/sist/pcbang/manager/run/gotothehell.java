00367d17791ce8adfabc3356a18ebbb2d4142079
Kim-JungWoon <45754698+Kim-JungWoon@users.noreply.github.com>
2019-02-14 10:26:35 +0900

    ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ

--- kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/run/gotothehell.java
+++ kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/run/gotothehell.java
@@ -5,7 +5,7 @@ public class gotothehell {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("헬프미");
-		System.out.println("안됩니다!!!!!!");
+		System.out.println("안됩니다!!!!!!");//ㅗㅗㅗㅗ
 	}
 
 }
