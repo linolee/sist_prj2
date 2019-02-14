6f30ff986cd7f5de9365b94551f89ae6e32373dc
eunyoung00 <beebee6656@gmail.com>
2019-02-14 10:20:02 +0900

    ㅠㅠ?

--- kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/controller/eunyoung.java
+++ kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/controller/eunyoung.java
@@ -3,7 +3,7 @@ package kr.co.sist.pcbang.manager.controller;
 public class eunyoung {
 
 	public static void main(String[] args) {
-		System.out.println("된다~~");
+		System.out.println("아니 안됀다ㅠㅠㅠ");
 	}
 
 }
