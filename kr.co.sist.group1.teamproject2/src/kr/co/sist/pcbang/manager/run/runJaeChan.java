3bf6d37d4e9180524320ad7a1ceded512139cda9
owner <owner@DESKTOP-RO725E7>
2019-02-14 10:17:07 +0900

    재찬 연습

---
+++ kr.co.sist.group1.teamproject2/src/kr/co/sist/pcbang/manager/run/runJaeChan.java
@@ -0,0 +1,10 @@
+package kr.co.sist.pcbang.manager.run;
+
+public class runJaeChan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Á¹·Á");
+	}
+
+}
